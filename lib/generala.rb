f079efcd275a0cfc25e2509c3f748a5137a18326
Kleer LA <kleer@kleer-sandbox.(none)>
2016-06-23 15:11:06 -0300

    jugador tirada 5 dados

--- lib/generala.rb
+++ lib/generala.rb
@@ -1,13 +1,20 @@
 class Generala
 	def initialize
-		@puntosJugador1 = 0
-	end
-	
-	def obtenerPuntosJugador1
-		return @puntosJugador1
+		@dados = [0,0,0,0,0]
 	end
 
 	def tirarDado
 		return 1 + rand(6)
+	end
+
+	def tirada
+		5.times do |i|
+			@dados[i] = tirarDado
+		end
+		return @dados
+	end
+
+	def imprimirTirada
+		@dados.to_s
 	end
 end
--- spec/generala_spec.rb
+++ spec/generala_spec.rb
@@ -3,12 +3,19 @@ require_relative "../lib/generala.rb"
 describe "Generala" do
 	it "se inicia la partida" do
 		partida = Generala.new
-		partida.obtenerPuntosJugador1.should == 0
 	end
 	
-	it "tirar un dado entre el 1 y el 6" do
+	it "carga valor en el primer dado jugador 1" do
 		partida = Generala.new
-		partida.tirarDado.should be_between(1, 6)
+		retorno = partida.tirada
+		retorno[0].should be_between(1,6) 
 	end
+
+	it "carga valor en el ultimo dado jugador 1" do
+		partida = Generala.new
+		retorno = partida.tirada
+		retorno[4].should be_between(1,6) 
+	end
+
 
 end
